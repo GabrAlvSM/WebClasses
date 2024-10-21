e9241c61a0ad25b7246b6c90dbcc32b6d3bcc2a4
GabrAlvSM <gabriel.alvin7@gmail.com>
2024-10-21 08:16:54 -0400

    The last one

--- Aulas-JS/listaExerciciosJS/10.ex.js
+++ Aulas-JS/listaExerciciosJS/10.ex.js
@@ -1,7 +1,24 @@
 // 10. Escreva um algoritmo que calcule e imprima a tabuada do 2,3,4,5,6,7,8,9,10 (1 a 10).
 
-let base, multiplicador = "";
+let num1 = 1;
+let num2 = 1;
+let resp = 0;
 
-for(i=0;i<1000;i++){
+do {
     
+    // console.log(num1)
+
+    while (num2 < 11) {
+        // resp = (num1 * num2)
+        console.log(num1, "*", num2, "=", (resp = (num1 * num2)))
+    
+        num2 = (num2 + 1)
+    }
+    
+    num1 = (num1 + 1);
+    num2 = 1;
+    console.log("--------")
 }
+while (num1 < 11)
+
+console.log("Fim!")
